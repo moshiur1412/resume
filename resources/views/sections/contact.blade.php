ff2e637511102f8a2fd7543be61586a75e03880e
vorsurm <moshiur1412@gmail.com>
2020-10-13 15:06:13 +0600

    Implemented the design template

---
+++ resources/views/sections/contact.blade.php
@@ -0,0 +1,50 @@
+<h2>Contact</h2>
+<p class="lead">
+	&ldquo;Coming together is the beginning. Keeping together is progress. Working together is success. &rdquo; - Henry Ford
+</p>
+
+<hr />
+
+<div class="row">
+	<div class="col-md-6">
+		<ul class="no-bullets">
+			<li>
+				<a href="twitter.com/vorsurm" target="_blank">
+					<span class="icon icon-twitter"></span>
+					http://twitter.com/vorsurm
+				</a>
+			</li>
+			<li>
+				<a href="linkedin.com/vorsurm" target="_blank">
+					<span class="icon icon-linkedin"></span>
+					http://linked.com/vorsurm
+				</a>
+			</li>
+		</ul>
+	</div>
+	<div class="col-md-6">
+		<ul class="no-bullets">
+			<li>
+				<a href="javascript:void(0);">
+					<span class="icon icon-skype"></span>
+					http::/skype.com/moshiur 
+				</a>
+			</li>
+			<li>
+				<a href="mailto:moshiur.rah2016@gmail.com">
+					<span class="icon icon-email"></span>
+					<span style="unicode-bidi:bidi-override;  ">
+						moshiur.rah2016@gmail.com
+					</span>
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>
+
+<hr />
+
+<div class="text-center">
+
+	<iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fvorsurm%3Fref%3Dfacebook&amp;width=120&amp;height=21&amp;colorscheme=dark&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;send=false&amp;appId=143175172374579" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:120px; height:21px;" allowtransparency="true"></iframe>
+</div>
