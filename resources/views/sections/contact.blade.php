e5c903fb10223dd9210209b6a39d20e16650d4fc
vorsurm <moshiur1412@gmail.com>
2020-10-15 14:05:19 +0600

    Updated facebook traget blank

--- resources/views/sections/contact.blade.php
+++ resources/views/sections/contact.blade.php
@@ -11,29 +11,29 @@
 			<li>
 				<a href="https://twitter.com/vorsurm" target="_blank">
 					<span class="icon icon-twitter"></span>
-					
 				</a>
 			</li>
+
 			<li>
 				<a href="https://www.linkedin.com/in/vorsurm" target="_blank">
 					<span class="icon icon-linkedin"></span>
-					
 				</a>
 			</li>
 
 			<li>
 				<a href="skype:vorsurm?userinfo">
 					<span class="icon icon-skype"></span>
-					
 				</a>
 			</li>
+
 			<li>
 				<a href="mailto:moshiur.rah2016@gmail.com">
 					<span class="icon icon-email"></span> 
 				</a>
 			</li>
+			
 			<li>
-				<a href="https://facebook.com/vorsurm">
+				<a href="https://facebook.com/vorsurm" target="_blank">
 					<span class="icon icon-facebook"></span> 
 				</a>
 			</li>
