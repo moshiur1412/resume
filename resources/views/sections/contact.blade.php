12212ce994ffcf46171606eb2bcc5200ab0332f5
vorsurm <moshiur1412@gmail.com>
2020-10-15 18:38:36 +0600

    fixed the mobile resposive problem

--- resources/views/sections/contact.blade.php
+++ resources/views/sections/contact.blade.php
@@ -5,8 +5,8 @@
 
 <hr />
 <div class="row">
-	<div class="col-md-8 col-md-offset-4 col-sm-offset-2">
-		<!-- <div class="social_icon"> -->
+	<!-- <div class="col-md-8 col-md-offset-4 col-sm-offset-2"> -->
+		<div class="social_icon">
 			<ul class="no-bullets">
 				<li> 
 					<a href="https://twitter.com/vorsurm" target="_blank">
